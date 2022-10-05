8a96e322c5feb25d034a8cf3312c7d48b552d904
KartikeSingh <gwk24yt@gmail.com>
2022-10-05 10:22:30 +0530

    base

---
+++ main.js
@@ -0,0 +1,33 @@
+const name = window.location.href.split("name=")[1];
+const root = document.getElementById("root");
+
+if (!name) {
+    root.innerHTML = `<div class="form">
+    <input type="text" id="name">
+    <button id="wishButton">Wish!</button> 
+    </div>`;
+
+    document.getElementById("wishButton").onclick = () => {
+        const name = document.getElementById('name')?.value;
+
+        if (!name) return alert('Fill the name first!');
+
+        const newUrl = window.location.pathname + `?name=${name}`;
+
+        navigator.clipboard.writeText(newUrl);
+
+        alert("Birthday Wish URL Copied!")
+
+        window.location.href = newUrl;
+    }
+} else {
+    root.innerHTML = `
+    <h1> Happy Birthday ${name}!</h1>
+    <img src="cake.png" id="cake">
+`;
+
+    const cake = document.getElementById("cake");
+
+    cake.setAttribute("width", 2627/8);
+    cake.setAttribute("height", 2846/8);
+}
